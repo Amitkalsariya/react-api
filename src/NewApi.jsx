b8368b2b41899b9c53f2243cbe4182b4705f3adc
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-07-05 17:50:49 +0530

    Done

--- src/NewApi.jsx
+++ src/NewApi.jsx
@@ -1,17 +1,18 @@
 import axios from 'axios'
+import { Field, Form, Formik } from 'formik'
 import React, { useEffect, useState } from 'react'
 
 const NewApi = () => {
     const [data, setdata] = useState([])
-    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE3MjAwMDc1ODM2NDMtODk5OTA3MDI2IiwiaWF0IjoxNzIwMDA3NTgzLCJleHAiOjE3MjAxODAzODN9.7RKs-BDLgp1TLtQ7jnnLonVCSZEBp_kDmOfDIfE5JX0"
+        const token = "i1720176543283ypa501624831mv"
     useEffect(()=>{
         test()
     },[])
     function test() {
-        axios.get("https://service.apikeeda.com/movie", {
+        axios.get("https://service.apikeeda.com/api/v1/gst-invoice", {
             headers: {
-                Authorization: token
-            }   
+                 "x-apikeeda-key" : token
+                }   
         })
         .then((res)=>{
             console.log(res.data.data);
@@ -21,28 +22,74 @@ const NewApi = () => {
             console.log(er);
         })
     }
+
+    const handleSubmit = (values) => {
+        console.log(values);
+        axios.post('https://service.apikeeda.com/api/v1/gst-invoice',values,{
+            headers: {
+                "x-apikeeda-key" : token, 
+           }
+        })
+        .then((res) => {
+            console.log("success");
+            test();
+        })
+        .catch((error) =>{
+            console.log(error);
+        })
+    }
+   
     
     
     return (
         <div>
+
+            <Formik
+                initialValues={{
+                    name : '',
+                    gstno : '',
+                    baseamount : '',
+                    gstpercentage : '',
+                    totalamount : '',
+                    invoiceno : ''
+                }}
+                onSubmit={handleSubmit}
+            >
+                <Form>
+                    name :- <Field type="text" name="name"></Field><br /><br />
+                    gstno :- <Field type="text" name="gstno"></Field><br /><br />
+                    baseamount :- <Field type="text" name="baseamount"></Field><br /><br />
+                    gstpercentage :- <Field type="text" name="gstpercentage"></Field><br /><br />
+                    totalamount :- <Field type="text" name="totalamount"></Field><br /><br />
+                    invoiceno :- <Field type="text" name="invoiceno"></Field><br /><br />
+                    <button type='submit'>Submit</button>
+                </Form>
+            </Formik>
+
                 <table border={1}>
                     <tr>
-                        <th>PosterURl</th>
-                        <th>Movie </th>
-                        <th>Time</th>
-                        <th>Available</th>
-                        <th>Type</th>
-                        <th>Review</th>
+                        <th>Id</th>
+                        <th>Name </th>
+                        <th>GST NO</th>
+                        <th>Base Amt</th>
+                        <th>per</th>
+                        <th>Total Amt</th>
+                        <th>Invoice No</th>
+                        <th>N</th>
+                     
                     </tr>
                     {
                         data.map((el,i)=>(
                             <tr>
-                                <td>{el.posterURL}</td>
-                                <td>{el.movieName}</td>
-                                <td>{el.runningTime}</td>
-                                <td>{el.availableOn}</td>
-                                <td>{el.movieType}</td>
-                                <td>{el.imdbReview}</td>
+                               <td>{el._id}</td>
+                               <td>{el.name}</td>
+                               <td>{el.gstno}</td>
+                               <td>{el.baseamount}</td>
+                               <td>{el.gstpercentage}</td>
+                               <td>{el.totalamount}</td>
+                               <td>{el.invoiceno}</td>
+                               <td>{el.__v}</td>
+                             
                             </tr>
                         ))
                     }
