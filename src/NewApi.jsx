c9f4300075117850178f358631e84e8fd4c2159e
AMIT <156776217+Amitkalsariya@users.noreply.github.com>
2024-07-09 17:43:39 +0530

    Done

--- src/NewApi.jsx
+++ src/NewApi.jsx
@@ -4,70 +4,118 @@ import React, { useEffect, useState } from 'react'
 
 const NewApi = () => {
     const [data, setdata] = useState([])
-        const token = "a1720436012007jyy624823406dp"
-    useEffect(()=>{
+    const [initialvalues , setinItialvalues] = useState({
+        name: '',
+        gstno: '',
+        baseamount: '',
+        gstpercentage: '',
+        totalamount: '',
+        invoiceno: ''
+    })
+    const [id,setId]=useState(null)
+    const token = "a1720436012007jyy624823406dp"
+    useEffect(() => {
         test()
     },[])
     function test() {
         axios.get("https://service.apikeeda.com/api/v1/gst-invoice", {
             headers: {
-                 "x-apikeeda-key" : token
-                }   
+                "x-apikeeda-key": token
+            }
         })
-        .then((res)=>{
-            console.log(res.data.data);
-            setdata(res.data.data)
-        })
-        .catch((er)=>{
-            console.log(er);
-        })
+            .then((res) => {
+                console.log(res.data.data);
+                setdata(res.data.data)
+            })
+            .catch((er) => {
+                console.log(er);
+            })
     }
 
-    const handleSubmit = (values) => {
-        console.log(values);
-        axios.post('https://service.apikeeda.com/api/v1/gst-invoice',values,{
-            headers: {
-                "x-apikeeda-key" : token, 
-           }
-        })
-        .then((res) => {
-            console.log("success");
-            test();
-        })
-        .catch((error) =>{
-            console.log(error);
-        })
+    const handleSubmit = (values,{resetForm}) => {
+
+        if(id !== null)
+        {
+            axios.patch(`https://service.apikeeda.com/api/v1/gst-invoice/${id}`,values,{
+                headers: {
+                    "x-apikeeda-key": token,
+                }
+            })
+            .then((res) => {
+                console.log("success");
+                test();
+                
+
+                setId(null)
+              
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+
+        }
+        else
+        {
+            axios.post('https://service.apikeeda.com/api/v1/gst-invoice', values, {
+                headers: {
+                    "x-apikeeda-key": token,
+                }
+            })
+            .then((res) => {
+                console.log("success");
+                test();
+                resetForm();
+                
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+        }
+            setinItialvalues({
+                name: '',
+                gstno: '',
+                baseamount: '',
+                gstpercentage: '',
+                totalamount: '',
+                invoiceno: ''
+            })
     }
-    const deleteData = (_id) => {
-     
-        axios.delete(`https://service.apikeeda.com/api/v1/gst-invoice/${_id}`,{
+
+    const deleteData = (id) => {
+        console.log(id);
+        axios.delete("https://service.apikeeda.com/api/v1/gst-invoice/" + id, {
             headers: {
-                "x-apikeeda-key" : token, 
-           }
+                "x-apikeeda-key": token
+            }
         })
-        .then((res) => {
-            console.log("success");
-            test();
-        })
-        .catch((error) =>{
-            console.log(error);
-        })
+            .then((res) => {
+                console.log("success");
+                test()
+            })
+            .catch((error) => {
+                console.log(error);
+            })
     }
-   
+
+    const editData = (el , id) => {
+        setinItialvalues({
+        name: el.name,
+        gstno: el.gstno,
+        baseamount: el.baseamount,
+        gstpercentage: el.gstpercentage,
+        totalamount: el.totalamount,
+        invoiceno: el.invoiceno
+        })
+        setId(id)
+    }
     
-    
+
     return (
         <div>
 
             <Formik
-                initialValues={{
-                    name : '',
-                    gstno : '',
-                    baseamount : '',
-                    gstpercentage : '',
-                    totalamount : '',
-                    invoiceno : ''
-                }}
+            enableReinitialize
+                initialValues={initialvalues}
                 onSubmit={handleSubmit}
             >
                 <Form>
@@ -81,36 +129,39 @@ const NewApi = () => {
                 </Form>
             </Formik>
 
-                <table border={1}>
-                    <tr>
-                        <th>Id</th>
-                        <th>Name </th>
-                        <th>GST NO</th>
-                        <th>Base Amt</th>
-                        <th>per</th>
-                        <th>Total Amt</th>
-                        <th>Invoice No</th>
-                        <th>N</th>
-                        <th>Delete</th>
-                     
-                    </tr>
-                    {
-                        data.map((el,i)=>(
-                            <tr>
-                               <td>{el._id}</td>
-                               <td>{el.name}</td>
-                               <td>{el.gstno}</td>
-                               <td>{el.baseamount}</td>
-                               <td>{el.gstpercentage}</td>
-                               <td>{el.totalamount}</td>
-                               <td>{el.invoiceno}</td>
-                               <td>{el.__v}</td>
-                               <td><button onClick={()=>{deleteData(el._id)}}>Delete</button></td>
-                             
-                            </tr>
-                        ))
-                    }
-                </table>
+            <table border={1}>
+                <tr>
+                    <th>Id</th>
+                    <th>Name </th>
+                    <th>GST NO</th>
+                    <th>Base Amt</th>
+                    <th>per</th>
+                    <th>Total Amt</th>
+                    <th>Invoice No</th>
+                    <th>N</th>
+                    <th>Delete</th>
+                    <th>Update</th>
+
+                </tr>
+                {
+                    data.map((el, i) => (
+                        <tr>
+                            <td>{el._id}</td>
+                            <td>{el.name}</td>
+                            <td>{el.gstno}</td>
+                            <td>{el.baseamount}</td>
+                            <td>{el.gstpercentage}</td>
+                            <td>{el.totalamount}</td>
+                            <td>{el.invoiceno}</td>
+                            <td>{el.__v}</td>
+                            <td>
+                                <button onClick={() => deleteData(el._id)}>Delete</button>
+                            </td>
+                            <td><button onClick={() => editData(el,el._id)}>Update</button></td>
+                        </tr>
+                    ))
+                }
+            </table>
         </div>
     )
 }
